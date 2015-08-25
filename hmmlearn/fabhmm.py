b76f5ae40a03bd1cb6f7b3eaee43e86bc4658da8
seataK <297.1951@gmail.com>
2015-08-26 01:14:59 +0900

    refactor

--- hmmlearn/fabhmm.py
+++ hmmlearn/fabhmm.py
@@ -11,6 +11,19 @@ from .utils import normalize, logsumexp, iter_from_X_lengths
 
 
 class MultinomialFABHMM(MultinomialHMM):
+    def __init__(self, n_components=10,
+                 startprob_prior=1.0, transmat_prior=1.0,
+                 algorithm="viterbi", random_state=None,
+                 n_iter=10, tol=1e-2, verbose=False,
+                 params="sted", init_params="sted"):
+        MultinomialHMM.__init__(self, n_components,
+                                startprob_prior=startprob_prior,
+                                transmat_prior=transmat_prior,
+                                algorithm=algorithm,
+                                random_state=random_state,
+                                n_iter=n_iter, tol=tol, verbose=verbose,
+                                params=params, init_params=init_params)
+
     def fit(self, X, lengths=None):
         """Estimate model parameters.
 
@@ -42,10 +55,12 @@ class MultinomialFABHMM(MultinomialHMM):
         self.posteriors = np.ones((n_samples,
                                   self.n_components)) / self.n_components
 
+        count = 0
         n_seqs = 1 if lengths is None else len(lengths)
+
         for iter in range(self.n_iter):
             stats = self._initialize_sufficient_statistics()
-            ficlb = 0
+            curr_logprob = 0
 
             shrinker, sh_reg = self._compute_shrinker(self.posteriors)
 
@@ -55,9 +70,7 @@ class MultinomialFABHMM(MultinomialHMM):
                     self._compute_shrinked_logprob(framelogprob, shrinker)
                 logprob, fwdlattice = \
                     self._do_forward_pass(shrinked_framelogprob)
-                ficlb += logprob
-                ficlb += sh_reg[1]
-                ficlb += sh_reg[0] * (j - i - 1)
+                curr_logprob += logprob
                 bwdlattice = self._do_backward_pass(shrinked_framelogprob)
                 posteriors = self._compute_posteriors(fwdlattice, bwdlattice)
                 self.posteriors[i:j] = posteriors
@@ -67,25 +80,37 @@ class MultinomialFABHMM(MultinomialHMM):
                     posteriors, fwdlattice,
                     bwdlattice, self.params)
 
-            D_beta = self.n_components
-            D_phi = self.n_features
-
             self._do_mstep(stats, self.params)
 
+            old_count = count
             count = self._eliminate_components()
 
-            ficlb -= D_beta / 2.0 * np.log(n_seqs)
-            sum_post_minus = np.sum(self.posteriors[:-1], axis=0)
-            sum_post = sum_post_minus + self.posteriors[-1]
-            term_beta = D_beta / 2.0 * (np.log(sum_post_minus) - 1)
-            term_phi = D_phi / 2.0 * (np.log(sum_post) - 1)
-            ficlb -= np.sum(term_beta + term_phi)
+            ficlb = self._compute_ficlb(curr_logprob, sh_reg,
+                                        n_samples, n_seqs)
 
             self.monitor_.report(ficlb)
-            if self.monitor_.converged and count is 0:
+            if self.monitor_.converged and count + old_count is 0:
                 break
 
         return self
+
+    def _compute_ficlb(self, logprob, sh_reg, n_samples, n_seqs):
+        ficlb = logprob
+        ficlb += sh_reg[0] * (n_samples - n_seqs) + sh_reg[1] * n_seqs
+        ficlb += - self.n_components / 2.0 * np.log(n_seqs)
+        _sum_post = np.sum(self.posteriors[:-1], axis=0)
+        sum_post = _sum_post + self.posteriors[-1]
+        term_beta = self.n_components / 2.0 * (np.log(_sum_post) - 1)
+        term_phi = self.dim_emission / 2.0 * (np.log(sum_post) - 1)
+        ficlb -= np.sum(term_beta + term_phi)
+
+        return ficlb
+
+    def _init(self, X, lengths=None, params='sted'):
+        super(MultinomialFABHMM, self)._init(X, lengths=lengths, params=params)
+
+        if 'd' in params:
+            self.dim_emission = self.n_features
 
     def _compute_shrinker(self, posteriors):
         n_samples, n_components = posteriors.shape
@@ -94,8 +119,8 @@ class MultinomialFABHMM(MultinomialHMM):
         sum_post = sum_post_minus + posteriors[-1]
 
         term_beta = - n_components / (2 * sum_post_minus)
-        # n_components should be n_demension of emmision parameter
-        term_phi = - self.n_features / (2 * sum_post)
+
+        term_phi = - self.dim_emission / (2 * sum_post)
 
         _logshrinker = np.zeros((2, self.n_components))
         _logshrinker[0] = term_beta + term_phi
@@ -135,5 +160,4 @@ class MultinomialFABHMM(MultinomialHMM):
         normalize(self.startprob_)
         normalize(self.startprob_)
 
-        print(self.n_components)
         return count
